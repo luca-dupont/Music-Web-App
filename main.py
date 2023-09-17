e4480913a313cb5b1388abb4ced2bca001163eef
lucaskye <1902060@brebeuf.qc.ca>
2023-09-17 14:44:07 -0400

    Fix file renaming

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def download_vid(url) :
         video = yt.streams.filter(only_audio=True).first()
         out_file = video.download(output_path="./downloads/")
         base, ext = os.path.splitext(out_file)
-        new_file = ''.join(letter for letter in base if letter.isalnum() or letter == '/' or letter == ' ') + '.mp3'
+        new_file = ''.join(base) + '.mp3'
         os.rename(out_file, new_file)
         
     else :
